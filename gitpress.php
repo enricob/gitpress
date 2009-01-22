c66499bbda5cf34f56a333161665eb08c48051b2
Enrico Bianco <enricob@gmail.com>
2009-01-22 13:15:24 -0500

    Now a multi-widget!  Also, no longer fails for users with no repositories.

--- gitpress.php
+++ gitpress.php
@@ -12,14 +12,24 @@ function gitpress_widget_init() {
     if ( !function_exists('register_sidebar_widget') || !function_exists('register_widget_control') )
 		return;
 	
-	function gitpress_widget($args) {
+	function gitpress_widget($args, $widget_args = 1) {
 	    
-	    extract($args);
+	    // Figure out which instance we're rendering
+	    extract( $args, EXTR_SKIP );
+    	if ( is_numeric($widget_args) )
+    		$widget_args = array( 'number' => $widget_args );
+    	$widget_args = wp_parse_args( $widget_args, array( 'number' => -1 ) );
+    	extract( $widget_args, EXTR_SKIP );
+
+    	// Bail out if we don't have options for this instance
+    	$options = get_option('gitpress_widget_options');
+    	if ( !isset($options[$number]) )
+    		return;
 	    
 	    // TODO: Add some more options (maybe some text to precede the repos list?)
 	    $options = get_option('gitpress_widget_options');
-	    $title = $options['title'];
-	    $username = $options['username'];
+	    $title = $options[$number]['title'];
+	    $username = $options[$number]['username'];
 	    
 	    // Grab the user info using the GitHub API
 	    $curl_session = curl_init();
@@ -33,39 +43,121 @@ function gitpress_widget_init() {
 	    
 	    
 	    echo $before_widget . $before_title . $title . $after_title;
-	    echo "<ul id='gitpress-repo-list'>";
-	    foreach ($repos as $repo) {
-	        $repo_name = $repo->{"name"};
-	        $repo_url = $repo->{"url"};
-	        echo "<li class='gitpress-repo'><a href='" . $repo_url . "'>" . $repo_name . "</a></li>";
-	    }
+	    echo "<ul id='gitpress-repo-list-" . $number . "' class='widget gitpress-repo-list'>";
+	    if (isset($repos)) {
+	        foreach ($repos as $repo) {
+    	        $repo_name = $repo->{"name"};
+    	        $repo_url = $repo->{"url"};
+    	        echo "<li class='gitpress-repo'><a href='" . $repo_url . "'>" . $repo_name . "</a></li>";
+    	    }
+        }
 	    echo "</ul>";
 	    echo $after_widget;
 	}
 	
-	function gitpress_widget_control() {
+	function gitpress_widget_control($widget_args = 1) {
 	    
-	    $options = get_option('gitpress_widget_options');
-	    if (!is_array($options)) {
-	        $options = array('title'=>'', 'username'=>'');
-	    }
-	    
-	    if ( $_POST['gitpress-submit'] ) {
-			$options['title'] = strip_tags(stripslashes($_POST['gitpress-title']));
-			$options['username'] = strip_tags(stripslashes($_POST['gitpress-username']));
-			update_option('gitpress_widget_options', $options);
-		}
+	    global $wp_registered_widgets;
+    	static $updated = false; // Whether or not we have already updated the data after a POST submit
+
+        // Figure out which instance we're working with
+    	if ( is_numeric($widget_args) )
+    		$widget_args = array( 'number' => $widget_args );
+    	$widget_args = wp_parse_args( $widget_args, array( 'number' => -1 ) );
+    	extract( $widget_args, EXTR_SKIP );
+
+    	// Create an options array if we don't have one
+    	$options = get_option('gitpress_widget_options');
+    	if ( !is_array($options) )
+    		$options = array();
+    		
+    	// Do we need to update the data?
+    	if ( !$updated && !empty($_POST['sidebar']) ) {
+    		// Which sidebar are we getting added to?
+    		$sidebar = (string) $_POST['sidebar'];
+
+    		$sidebars_widgets = wp_get_sidebars_widgets();
+    		if ( isset($sidebars_widgets[$sidebar]) )
+    			$this_sidebar =& $sidebars_widgets[$sidebar];
+    		else
+    			$this_sidebar = array();
+
+    		foreach ( $this_sidebar as $_widget_id ) {
+    			// Remove all widgets of this type from the sidebar.  We'll add the new data in a second.  This makes sure we don't get any duplicate data
+    			// since widget ids aren't necessarily persistent across multiple updates
+    			if ( 'gitpress_widget' == $wp_registered_widgets[$_widget_id]['callback'] && isset($wp_registered_widgets[$_widget_id]['params'][0]['number']) ) {
+    				$widget_number = $wp_registered_widgets[$_widget_id]['params'][0]['number'];
+    				if ( !in_array( "gitpress-$widget_number", $_POST['widget-id'] ) ) // the widget has been removed.
+    					unset($options[$widget_number]);
+    			}
+    		}
+            
+            // Update options if we're processing a POST
+    		foreach ( (array) $_POST['gitpress-widget'] as $widget_number => $gitpress_widget_instance ) {
+    			if ( (!isset($gitpress_widget_instance['title']) || !isset($gitpress_widget_instance['username']))
+    			        && isset($options[$widget_number]) )
+    			        // user clicked cancel
+    				    continue;
+    			$title = strip_tags(stripslashes($gitpress_widget_instance['title']));
+    			$username = strip_tags(stripslashes($gitpress_widget_instance['username']));
+    			$options[$widget_number] = array( 'title' => $title, 'username' => $username );
+    		}
+
+    		update_option('gitpress_widget_options', $options);
+
+    		$updated = true; // So that we don't go through this more than once
+    	}
+    	
+    	if ( -1 == $number ) {
+    		$title = '';
+    		$username = '';
+    		$number = '%i%';    // temporary number to be replaced by JS
+    	} else {
+    		$title = attribute_escape($options[$number]['title']);
+    		$username = attribute_escape($options[$number]['username']);
+    	}
 		
-		$title = htmlspecialchars($options['title'], ENT_QUOTES);
-		$username = htmlspecialchars($options['username'], ENT_QUOTES);
+		$title_id = 'gitpress-title-' . $number;
+		$title_name = 'gitpress-widget[' . $number . '][title]';
+		$username_id = 'gitpress-username-' . $number;
+		$username_name = 'gitpress-widget[' . $number . '][username]';
+		$submit_id = 'gitpress-submit-' . $number;
+		$submit_name = 'gitpress-widget[' . $number . '][submit]';
 		
-		echo '<p style="text-align:right;"><label for="gitpress-title">' . __('Title:') . ' <input style="width: 200px;" id="gitpress-title" name="gitpress-title" type="text" value="'.$title.'" /></label></p>';
-		echo '<p style="text-align:right;"><label for="gitpress-username">' . __('User:') . ' <input style="width: 200px;" id="gitpress-username" name="gitpress-username" type="text" value="'.$username.'" /></label></p>';
-		echo '<input type="hidden" id="gitpress-submit" name="gitpress-submit" value="1" />';
+		echo '<p style="text-align:right;"><label for="' . $title_id . '">' . __('Title:') . ' <input style="width: 200px;" id="' . $title_id . '" name="' . $title_name . '" type="text" value="' . $title .'" /></label></p>';
+		echo '<p style="text-align:right;"><label for="' . $username_id . '">' . __('User:') . ' <input style="width: 200px;" id="' . $username_id . '" name="' . $username_name . '" type="text" value="' . $username . '" /></label></p>';
+		echo '<input type="hidden" id="' . $submit_id . '" name="' . $submit_name . '" value="1" />';
 	}
 	
-	register_sidebar_widget('GitPress', 'gitpress_widget');
-	register_widget_control('GitPress', 'gitpress_widget_control');
+	function gitpress_widget_register() {
+	    if ( !$options = get_option('gitpress_widget_options') )
+    		$options = array();
+
+    	$widget_ops = array('classname' => 'gitpress_widget', 'description' => __('Widget which allows multiple instances'));
+    	$control_ops = array('id_base' => 'gitpress');
+    	$name = __('GitPress');
+
+    	$registered = false;
+    	foreach ( array_keys($options) as $o ) {
+    		// Old widgets can have null values for some reason
+    		if ( !isset($options[$o]['title']) || !isset($options[$o]['username']) )
+    			continue;
+
+    		// $id should look like {$id_base}-{$o}
+    		$id = "gitpress-$o"; // Never never never translate an id
+    		$registered = true;
+    		wp_register_sidebar_widget( $id, $name, 'gitpress_widget', $widget_ops, array( 'number' => $o ) );
+    		wp_register_widget_control( $id, $name, 'gitpress_widget_control', $control_ops, array( 'number' => $o ) );
+    	}
+
+    	// If there are none, we register the widget's existance with a generic template
+    	if ( !$registered ) {
+    		wp_register_sidebar_widget( 'gitpress-1', $name, 'gitpress_widget', $widget_ops, array( 'number' => -1 ) );
+    		wp_register_widget_control( 'gitpress-1', $name, 'gitpress_widget_control', $control_ops, array( 'number' => -1 ) );
+    	}
+	}
+	
+	gitpress_widget_register();
 }
 
 add_action('widgets_init', 'gitpress_widget_init');
